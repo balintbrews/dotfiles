bdd5872b8ce2348d8ee80dd11d0cea0ae3bc730a
Balint Kleri <balint@kleri.hu>
2016-12-28 23:53:05 +0100

    Download Dracula theme for Terminal.app

--- install.sh
+++ install.sh
@@ -13,3 +13,5 @@ git clone https://github.com/magicmonty/bash-git-prompt.git "$HOME/.dotfiles_lib
 git clone https://github.com/VundleVim/Vundle.vim.git "$HOME/.vim/bundle/Vundle.vim"
 # Vim plugins
 vim +PluginInstall +qall
+# Dracula theme for Terminal.app
+curl -o "$HOME/.dotfiles_lib/Dracula.terminal" https://raw.githubusercontent.com/dracula/terminal.app/master/Dracula.terminal
