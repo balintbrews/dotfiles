7aa1edae67376be9750e79356798b4d73b9eeb50
Balint Kleri <b@balintbrews.com>
2026-02-14 15:39:19 +0100

    Remove git stash info from prompt

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -4,6 +4,7 @@ override_git_prompt_colors() {
   GIT_PROMPT_STAGED="${Yellow}● "
   GIT_PROMPT_UNTRACKED="${Cyan}… "
   GIT_PROMPT_STASHED="${BoldMagenta}⚑ "
+  GIT_PROMPT_IGNORE_STASH=1
   GIT_PROMPT_CLEAN="${Green}✔ "
   GIT_PROMPT_COMMAND_OK="${Green}✔ "
   GIT_PROMPT_COMMAND_FAIL="${Red}✘ "
