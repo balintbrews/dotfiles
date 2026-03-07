02869da8be464fd412662432e2ac6cef7cc98e36
Balint Kleri <b@balintbrews.com>
2026-03-07 22:08:35 +0100

    Update prompt with remote hostname

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -10,7 +10,7 @@ override_git_prompt_colors() {
   GIT_PROMPT_COMMAND_FAIL="${Red}✘ "
 
   Time12a="\$(date +%H:%M:%S)"
-  GIT_PROMPT_START_USER="${Yellow}${PathShort}"
+  GIT_PROMPT_START_USER="\$(if [ -n \"\${SSH_CONNECTION:-}\${SSH_CLIENT:-}\${SSH_TTY:-}\" ]; then host=\$(hostname -s 2>/dev/null); if [ -n \"\$host\" ]; then printf '${Cyan}[${BoldMagenta}%s${Cyan}] ' \"\$host\"; fi; fi)${Yellow}${PathShort}"
   GIT_PROMPT_START_ROOT="${GIT_PROMPT_START_USER}"
   GIT_PROMPT_END_USER="\n${BoldBlue}❯ ${ResetColor}"
   GIT_PROMPT_END_ROOT="${GIT_PROMPT_END_USER}"
